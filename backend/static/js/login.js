4b403d31600dbbcfece3f2905a3a0d0580708bd4
Karl <arandomdude789@outlook.com>
2025-01-11 15:33:25 -0500

    Form??

--- backend/static/js/login.js
+++ backend/static/js/login.js
@@ -1,6 +1,8 @@
 const form = document.getElementById("login");
 
-form.createEventListener(function (e) {
+form.createEventListener("submit", function (e) {
+	e.preventDefault();
+	
 	const inputs = document.getElementsByTagName("input");
 	const email = inputs[0].value;
 	const password = inputs[1].value;
