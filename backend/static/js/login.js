6296193891e1e9b0b448054a4a5f5c89c3918e57
Karl <arandomdude789@outlook.com>
2025-01-11 17:44:30 -0500

    login.js??????

--- backend/static/js/login.js
+++ backend/static/js/login.js
@@ -1,22 +1,10 @@
-const form = document.getElementById("login");
-
-form.addEventListener("submit", function (e) {
-	e.preventDefault();
-
-	const inputs = document.getElementsByTagName("input");
-	const email = inputs[0].value;
-	const password = inputs[1].value;
-	fetch("http://127.0.0.1:5000/login", {
-		method: "POST",
-		body: JSON.stringify({"email": email, "password": password}),
-		headers: {
-			"Content-Type": "application/json"
-		}
-	}).then((text) => {
-		if (text.ok && text.status == 200) {
-			return text.json();
-		}
-	}).then((json) => {
-		window.location.href = window.location.href.slice(0, window.location.href.indexOf("/")) + json.url;
-	});
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById("login");
+    
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        
+        // Instead of handling the login locally, redirect to the Auth0 login route
+        window.location.href = "/login";
+    });
 });
