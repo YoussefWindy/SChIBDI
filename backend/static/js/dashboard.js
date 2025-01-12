7622ee101274a9f3deffe53f61fe33582c6b2dd4
Karl <arandomdude789@outlook.com>
2025-01-11 23:36:45 -0500

    Added button :>

--- backend/static/js/dashboard.js
+++ backend/static/js/dashboard.js
@@ -3,22 +3,36 @@ date.addEventListener("click", function (e) {
 	window.location.href += "?date=" + document.getElementById("date_picker").value;
 });
 
-const breakfast = document.getElementById("break-submit");
+const breakfast = document.getElementById("break-add");
 breakfast.addEventListener("click", function (e) {
-	// Insert fetch function to database
+	
 });
 
-const lunch = document.getElementById("lunch-submit");
+const lunch = document.getElementById("lunch-add");
 lunch.addEventListener("click", function (e) {
 	// Insert fetch function to database
 });
 
-const dinner = document.getElementById("dinner-submit");
+const dinner = document.getElementById("dinner-add");
 dinner.addEventListener("click", function (e) {
 	// Insert fetch function to database
 });
 
-const snack = document.getElementById("snack-submit");
+const snack = document.getElementById("snack-add");
 snack.addEventListener("click", function (e) {
 	// Insert fetch function to database
+});
+
+const symptoms = document.getElementById("sym-add");
+symptoms.addEventListener("click", function (e) {
+	const symptom_inputs = document.getElementById("sym-inputs");
+	console.log(symptom_inputs)
+	const new_input = document.createElement("input");
+	new_input.type = "text";
+	new_input.classList.add("sympts");
+	new_input.name = "syms";
+	new_input.list = "symps";
+	new_input.placeholder = "Add a symptom";
+	symptom_inputs.appendChild(document.createElement("br"));
+	symptom_inputs.appendChild(new_input);
 });
