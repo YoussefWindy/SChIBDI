5bab0c3666b36bc3a802539783c3d08c3653902e
Karl <arandomdude789@outlook.com>
2025-01-12 00:23:56 -0500

    Committing date stuff

--- backend/static/js/dashboard.js
+++ backend/static/js/dashboard.js
@@ -1,6 +1,10 @@
-const date = document.getElementById("date");
-date.addEventListener("click", function (e) {
-	window.location.href += "?date=" + document.getElementById("date_picker").value;
+const pick_date = document.getElementById("date");
+pick_date.addEventListener("click", function (e) {
+	let date = document.getElementById("date_picker").value;
+	while (date.indexOf("-") !== -1) {
+		date = date.slice(0, date.indexOf("-")) + date.slice(date.indexOf("-") + 1);
+	}
+	window.location.href += "?date=" + date;
 });
 
 const breakfast = document.getElementById("break-add");
@@ -15,12 +19,28 @@ lunch.addEventListener("click", function (e) {
 
 const dinner = document.getElementById("dinner-add");
 dinner.addEventListener("click", function (e) {
-	// Insert fetch function to database
+	const symptom_inputs = document.getElementById("dinner-inputs");
+	console.log(symptom_inputs)
+	const new_input = document.createElement("input");
+	new_input.type = "text";
+	new_input.classList.add("dinner");
+	new_input.name = "dinner";
+	new_input.placeholder = "Dinner";
+	symptom_inputs.appendChild(document.createElement("br"));
+	symptom_inputs.appendChild(new_input);
 });
 
 const snack = document.getElementById("snack-add");
 snack.addEventListener("click", function (e) {
-	// Insert fetch function to database
+	const symptom_inputs = document.getElementById("snack-inputs");
+	console.log(symptom_inputs)
+	const new_input = document.createElement("input");
+	new_input.type = "text";
+	new_input.classList.add("snack");
+	new_input.name = "snack";
+	new_input.placeholder = "Yummy snack 😋";
+	symptom_inputs.appendChild(document.createElement("br"));
+	symptom_inputs.appendChild(new_input);
 });
 
 const symptoms = document.getElementById("sym-add");
